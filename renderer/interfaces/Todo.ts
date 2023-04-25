2c1e55f2079317bb00c0d960a319d6d625bd23fc
rottedbean <asdzxcv37@naver.com>
2023-04-25 11:04:29 +0900

    todo관련 인터페이스 생성

---
+++ renderer/interfaces/Todo.ts
@@ -0,0 +1,26 @@
+export interface Tag {
+    name: string
+}
+
+const state = {
+    Default: 'default',
+    Done: 'done',
+    Expire: 'expire',
+    Periodical: 'periodical',
+    Pending: 'pending'
+} as const
+
+export type State = typeof state[keyof typeof state]
+
+export interface Todo {
+    id: string
+    title:string
+    text:string
+    state: State
+    expire_date: null|Date
+    tag?: Tag[] 
+
+}
+
+
+
